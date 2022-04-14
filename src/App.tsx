4456db8bd1015befaa22a670570ba07fb25b7dfd
каримов расулбек <rasulbekkari008@gmail.com>
2022-04-14 20:48:24 +0600

    refuctoring

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   const [rightAnswers, setRightAnswers] = useState<number>(0);
   const questions = useSelector<RootType, questionTypes[]>(state => state)
   const [number, setNumber] = useState<number>(0);
-  const [showNext, setShowNext] = useState<boolean>(true)
+  const [showNext, setShowNext] = useState<boolean>(false)
   const [right, setRight] = useState<boolean>(false)
 
   function handleClickNext() {
@@ -36,7 +36,10 @@ function App() {
         <img className='quiz__img' src="http://testing-companies.com/wp-content/uploads/2017/05/%D0%B7%D0%B0%D0%B3%D1%80%D1%83%D0%B6%D0%B5%D0%BD%D0%BE-1.jpg" alt="" />
         <h1 className='quiz__title'>React QUIZ</h1>
         <button onClick={handleClickStart} className='quiz__start'>Start</button>
-        {/* <p className='quiz__score'>Score: {!showNext && rightAnswers}</p> */}
+        {
+          !showNext && <p className='quiz__score'>Score: {!showNext && rightAnswers}</p>
+        }
+
         {
           showNext ?
             (<><QuestionCard right={right} setRight={setRight} question={questions[number].question} answers={questions[number].answers} questionNumber={number + 1} totalQuestions={questions.length} rightAnswer={questions[number].rightAnswer} setRightAnswers={setRightAnswers} /><button onClick={handleClickNext} className='quiz__next'>Next</button> </>) : null
